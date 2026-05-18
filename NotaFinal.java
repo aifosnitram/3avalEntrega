6349f89b1d888576f50e356c19bafcc4d1c574cb
aifosnitram <aifosnitram@gmail.com>
2026-05-18 13:53:41 +0200

    refactor main file

--- NotaFinal.java
+++ NotaFinal.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class NotaTest {
+public class NotaFinal {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             //definicion de variables
